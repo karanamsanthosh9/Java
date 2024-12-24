52d49a1f5562a738acd49d84ffb75e5371747f03
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2024-12-24 06:01:53 +0530

    Update EnumDemo.java

--- Learnings/Java Advanced/EnumDemo.java
+++ Learnings/Java Advanced/EnumDemo.java
@@ -1,11 +1,22 @@
-enum RespCodes{
-    success = 200,
-    parsingError = 403,
-    systemFailure = 500
+
+enum ResponseCodes
+{
+    success,
+    parsingError ,
+    systemFailure 
 }
 
-
 public class EnumDemo
-{
-    
+{    
+    public static void main(String args[])
+    {    
+        System.out.println(ResponseCodes.success);
+        System.out.println(ResponseCodes.parsingError);
+        System.out.println(ResponseCodes.systemFailure);
+
+        ResponseCodes resp = ResponseCodes.success;
+        System.out.println(resp);
+        System.out.println(resp.ordinal()) ; //prints the index of the enum beeing assigned.
+        
+    }
 }
