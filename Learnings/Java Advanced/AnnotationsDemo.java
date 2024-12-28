e6c2e2105b3af7771ede0d79b184ae09040c1a0c
karanamsanthosh18 <karanamsanthosh18@gmail.com>
2024-12-28 17:17:50 +0000

    okay

--- Learnings/Java Advanced/AnnotationsDemo.java
+++ Learnings/Java Advanced/AnnotationsDemo.java
@@ -1,6 +1,23 @@
 public class AnnotationsDemo{
 
+    public void demo()
+    {
+        System.out.println("AnnotationsDemo - demo()");
+    }
 
+}
+
+// @Deprecated - to show/warn that context identifier is deprecated or better alternative is available
+
+class AnnotationsDemo_child extends AnnotationsDemo
+{
+    @Override
+    public void demo()
+    {
+        //this tells compiler explicitly that we are overriding the method
+        System.out.println("Annotations child - demo ");
+    
+    }
 }
 
 class AnnotationsDemo_Handler
@@ -9,5 +26,8 @@ class AnnotationsDemo_Handler
     public static void main(String arg[])
     {
         System.out.println("Main()");
+
+        AnnotationsDemo_child ob = new AnnotationsDemo_child();
+        ob.demo();
     }
 }
