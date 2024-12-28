b63cb01ffd8d2ee84e0728d2d251f88403bebeb2
karanamsanthosh18 <karanamsanthosh18@gmail.com>
2024-12-28 05:23:19 +0000

    commit

---
+++ Learnings/Practice.java
@@ -0,0 +1,17 @@
+public class Practice
+{
+    void F1()
+    {
+        System.out.println("this.name()" + this.name());
+    }
+}
+
+
+class Practice_Handler
+{
+    public static void main(String args[])
+    {
+        System.out.println("main");
+        //call
+    }
+}
