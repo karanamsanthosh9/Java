84500fa57c67e491bce9c6aacf0474aa0d280614
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-02 13:47:19 +0000

    practice

---
+++ Learnings/Practice.java
@@ -0,0 +1,28 @@
+
+
+abstract class Practice_Abstract
+{
+    void demo()
+    {
+        System.out.println("This is non-abstract method inside a abstract class");
+    }
+    abstract void demo2();
+}
+
+class Extended_Practice extends Practice_Abstract
+{
+    void demo2()
+    {
+        System.out.println("abstract method's implementation");
+    }
+}
+
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Main");
+        Practice_Abstract ob = new Extended_Practice();
+        ob.demo();
+        ob.demo2();
+    }     
+}
