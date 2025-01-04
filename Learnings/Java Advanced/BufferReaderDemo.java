32cd274fb49942de13398ed54131491e382a7f3a
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-04 10:22:14 +0000

    okay

---
+++ Learnings/Java Advanced/BufferReaderDemo.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+//import java.util.Scanner;
+public class BufferReaderDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Main");
+        Input
+        BufferedReader 
+    }     
+
+}
