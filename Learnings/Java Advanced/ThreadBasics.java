e7dea20f64b07aabcf7c2af47e6dbc1741cb4f4e
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-05 01:34:50 +0000

    threading

--- Learnings/Java Advanced/ThreadBasics.java
+++ Learnings/Java Advanced/ThreadBasics.java
@@ -9,6 +9,13 @@ class A_ThreadBasics extends Thread // now this is a thread, it should have a ru
     {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hii");
+            try
+            {
+            Thread.sleep(1);
+            }catch(Exception e)
+            {
+                System.out.println("exception :" + e );
+            }
         }
     }
 }
@@ -19,6 +26,13 @@ class B_ThreadBasics extends Thread
     {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello");
+            try
+            {
+            Thread.sleep(1);
+            }catch(Exception e)
+            {
+                System.out.println("exception :" + e );
+            }
         }
     }
 }
@@ -32,7 +46,9 @@ public class ThreadBasics {
         B_ThreadBasics ob_b = new B_ThreadBasics();
         System.out.println(ob_a.getPriority());//=> 5, range = 1 to 10
         // we can only suggest scheduler , but can't exactly tell to maintain exact priority
-        ob_a.setPriority(10);
+       // ob_a.setPriority(10);
+      //  ob_b.setPriority(10);// no guarantee that hello will be printed first, 
+        //we can only try to optimize the thread priority
         ob_a.start();//this will directly calls the run method
         ob_b.start();   
     }
