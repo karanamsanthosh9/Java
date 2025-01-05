96c67cfbf3697111ceabbea70e149cceae8264cc
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-05 01:27:59 +0000

    okay

--- Learnings/Java Advanced/ThreadBasics.java
+++ Learnings/Java Advanced/ThreadBasics.java
@@ -7,7 +7,7 @@ class A_ThreadBasics extends Thread // now this is a thread, it should have a ru
 {
     public void run()
     {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Hii");
         }
     }
@@ -17,7 +17,7 @@ class B_ThreadBasics extends Thread
 {
     public void run()
     {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Hello");
         }
     }
@@ -30,7 +30,9 @@ public class ThreadBasics {
         System.out.println("Main");
         A_ThreadBasics ob_a = new A_ThreadBasics();
         B_ThreadBasics ob_b = new B_ThreadBasics();
-        System.out.println(ob_a.getPriority());// we can only suggest scheduler , but can't exactly tell to maintain exact priority
+        System.out.println(ob_a.getPriority());//=> 5, range = 1 to 10
+        // we can only suggest scheduler , but can't exactly tell to maintain exact priority
+        ob_a.setPriority(10);
         ob_a.start();//this will directly calls the run method
         ob_b.start();   
     }
