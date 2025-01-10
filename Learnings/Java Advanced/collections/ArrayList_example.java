18bebf6fb438e0085b01195c4e882154a14fa00a
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-10 16:09:58 +0000

    arrayList

--- Learnings/Java Advanced/collections/ArrayList_example.java
+++ Learnings/Java Advanced/collections/ArrayList_example.java
@@ -1,4 +1,4 @@
-import java.security.cert.CollectionCertStoreParameters;
+
 import java.util.ArrayList;
 import java.util.Collection;
 //default imported package is java.lang
@@ -7,61 +7,94 @@ import java.util.Scanner;
 
 public class ArrayList_example{
 
-    public static void main(String[] args) {
-        System.out.println("Main");
-        arrayListDemo();
-    }
+	public static void main(String[] args) {
+		System.out.println("Main");
+		//arrayListDemo();
+		// arrayListDemo2();
+		arrayListDemo3();
+	}
 
-    static void arrayListDemo() {
-        // Collection col = new ArrayList();
-        // col.add(1);
-        // col.add(12);
-        // col.add(412);
-        // System.out.println("directly printing :" + col);
-        // till here, :
-        /*
-         * @karanamsanthosh9 ➜ /workspaces/Java/Learnings/Java Advanced (main) $ javac
-         * CollectionsDemo1.java
-         * Note: CollectionsDemo1.java uses unchecked or unsafe operations.
-         * Note: Recompile with -Xlint:unchecked for details.
-         * 
-         * output:
-         * java CollectionsDemo1
-         * Main
-         * [1, 12, 412]
-         */
-       // Collection<Integer> c = new ArrayList<Integer>(); //<> - are called generics.....
-       List<Integer> c = new ArrayList<>();
-        c.add(122);
-        c.add(123);
-        c.add(124);
-        System.out.println("Direct Priting  : " + c);
-        for(int i : c)
-        {
-            System.out.println(i);
-        }
-        c = null;
-        System.out.println("after null  : " + c); //c value coming as null
-        List<Integer> v2 = new ArrayList<Integer>();
+	static void arrayListDemo() {
+		// Collection col = new ArrayList();
+		// col.add(1);
+		// col.add(12);
+		// col.add(412);
+		// System.out.println("directly printing :" + col);
+		// till here, :
+		/*
+		 * @karanamsanthosh9 ➜ /workspaces/Java/Learnings/Java Advanced (main) $ javac
+		 * CollectionsDemo1.java
+		 * Note: CollectionsDemo1.java uses unchecked or unsafe operations.
+		 * Note: Recompile with -Xlint:unchecked for details.
+		 * 
+		 * output:
+		 * java CollectionsDemo1
+		 * Main
+		 * [1, 12, 412]
+		 */
+		// Collection<Integer> c = new ArrayList<Integer>(); //<> - are called generics.....
+		List<Integer> c = new ArrayList<>();
+		c.add(122);
+		c.add(123);
+		c.add(124);
+		System.out.println("Direct Priting  : " + c);
+		for(int i : c)
+		{
+			System.out.println(i);
+		}
+		c = null;
+		System.out.println("after null  : " + c); //c value coming as null
+	}
 
-        for(int i=0; i<100; i++)
-        {
-            v2.add(i,(Integer) (i*i) ) ;
-        }
+	static void arrayListDemo2(){
 
-        System.out.println(v2);
-        //System.out.println(v2.indexOf(3)); it will at what index 3 is there
-        System.out.print("Which square you want to know :");
-        Scanner sc = new Scanner(System.in);
-        int index = sc.nextInt();
-        //System.out.println("square of " + index + " is " + v2.indexOf(index)); wrong
-        System.out.println("square of " + index + " is " + v2.get(index)); 
+		List<Integer> v2 = new ArrayList<Integer>();
 
-        System.out.print("enter value whose sq root you want to find out  :");
-        int value = sc.nextInt();
-        System.out.println("square root of " + value + " is " + v2.indexOf(value));
-        sc.close();
+		for(int i=0; i<100; i++)
+		{
+			v2.add(i,(Integer) (i*i) ) ;
+		}
 
-        System.out.println("Size of v2 is : "  + v2.size());
-    }
+		System.out.println(v2);
+		//System.out.println(v2.indexOf(3)); it will at what index 3 is there
+		System.out.print("Which square you want to know :");
+		Scanner sc = new Scanner(System.in);
+		int index = sc.nextInt();
+		//System.out.println("square of " + index + " is " + v2.indexOf(index)); wrong
+		System.out.println("square of " + index + " is " + v2.get(index)); 
+
+		System.out.print("enter value whose sq root you want to find out  :");
+		int value = sc.nextInt();
+		System.out.println("square root of " + value + " is " + v2.indexOf(value));
+		sc.close();
+
+		System.out.println("Size of v2 is : "  + v2.size());
+		//System.out.println("Removing last element: "  + v2.remove(100)); got index 100 out of bounds exception
+		System.out.println("Removing last element: "  + v2.remove(99));
+		System.out.println("now Size of v2 is : "  + v2.size());
+		Integer arr[] = new Integer[100];
+		arr = (Integer[]) v2.toArray();
+		System.out.println(arr);
+
+	}
+
+	static void arrayListDemo3(){
+
+		List<Integer> v2 = new ArrayList<Integer>();
+
+		for(int i=0; i<100; i++)
+		{
+			v2.add(i,(Integer) (i*i) ) ;
+		}
+		//System.out.println(v2);
+		// System.out.println("Size of v2 is : "  + v2.size());
+		Integer arr[] = new Integer[100];
+		arr = (Integer[]) v2.toArray(arr);
+		//System.out.println(arr); some junk value
+		for(int i: arr)
+		{
+			System.out.println(i);
+		}
+
+	}
 }
