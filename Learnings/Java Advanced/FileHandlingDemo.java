77c5cce95a28258bde810258e14bbed132f4876b
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-14 06:48:25 +0000

    commit changes

--- Learnings/Java Advanced/FileHandlingDemo.java
+++ Learnings/Java Advanced/FileHandlingDemo.java
@@ -1,37 +1,43 @@
 
-
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class FileHandlingDemo {
-
     public static void main(String[] args) {
         System.out.println("Main of FileHandlingDemo()");
 
         try {
-        File f = new File("randomfile.class");
-        f.createNewFile();
-
+            File f = new File("randomfile.class");
+            f.createNewFile();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
+        try {
+            FileWriter fw = new FileWriter("randomfile.class");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter text to write to randomfile.class");
+            String input = "";
+            while(true)
+            {
+                System.out.println("Enter something:");
+                String temp = sc.nextLine();
+                input = input + temp;
+                if(temp.trim().isEmpty())
+                {
+                    break;
+                }
+            }
+            System.out.println(input);
+            fw.write(input);
+            fw.close();
 
-        try
-        {
-        FileWriter fw = new FileWriter("randomfile.class");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter text to write to randomfile.class");
-        String str = new String();
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            //
         }
-        while (sc.hasNext()) {
-            str = sc.nextLine();
-        }
-        fw.append(str);
-        
+
     }
 }
