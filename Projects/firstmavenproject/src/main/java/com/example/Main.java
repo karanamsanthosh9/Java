160dae4938eaa92d81e75f768ddb7e11caf31a28
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-25 15:11:27 +0000

    commit changes

--- Projects/firstmavenproject/src/main/java/com/example/Main.java
+++ Projects/firstmavenproject/src/main/java/com/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("inside main of Main.java");
+
+        System.out.println("Okay");
     }
 }
+
