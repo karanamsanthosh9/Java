03132484f106ec55675c132b3bb0a3aeffc0fd27
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-29 17:21:11 +0000

    commit changes

--- Projects/hibernate_project/src/main/java/com/example/Main.java
+++ Projects/hibernate_project/src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Student st = new Student();
+        st.setId(1);
+        st.setMarks(75);
+        st.setName("Kisan");
+
+        Configuration cfg = new Configuration();
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        session.save(st);
+
+        System.out.println("Done");
+
+
     }
 }
