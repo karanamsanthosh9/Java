490ac7b9bcc6930b57b725d08c36138824c2e327
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-02-06 15:42:00 +0000

    commit spring boot changes

--- first_spring_boot_project/src/main/java/com/example/first_spring_boot_project/FirstSpringBoot.java
+++ first_spring_boot_project/src/main/java/com/example/first_spring_boot_project/FirstSpringBoot.java
@@ -11,6 +11,7 @@ public class FirstSpringBoot {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBoot.class, args);
 		System.out.println("Hello world from SpringBoot day - 222");
+		
 		String s = "    Hello world from SpringBoot day - 2" ;
 		// String s = "";
 		System.out.println(s);
---
+++ first_spring_boot_project/src/main/java/com/example/first_spring_boot_project/FirstSpringBootController.java
@@ -0,0 +1,14 @@
+package com.example.first_spring_boot_project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FirstSpringBootController {
+    @RequestMapping(value="/")
+    public String index()
+    {
+        return "testing first application using controller...now modifying";
+    }
+}
