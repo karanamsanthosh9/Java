244cb444f280181feefce111a2114922f6bdbe0d
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-02-09 05:26:04 +0000

    new spring project

---
+++ Projects/basic_spring_project_artifactid/src/test/java/com/example/basic_spring_project_artifactid/BasicSpringProjectArtifactidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_spring_project_artifactid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringProjectArtifactidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
