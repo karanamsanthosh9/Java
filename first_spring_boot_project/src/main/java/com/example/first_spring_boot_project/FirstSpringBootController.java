02312513af8449fe2193d8c5b29c4d4401d70702
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-02-09 04:13:40 +0000

    commit

--- first_spring_boot_project/src/main/java/com/example/first_spring_boot_project/FirstSpringBootController.java
+++ first_spring_boot_project/src/main/java/com/example/first_spring_boot_project/FirstSpringBootController.java
@@ -1,15 +1,16 @@
 package com.example.first_spring_boot_project;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+// @RestController
 public class FirstSpringBootController {
-    @RequestMapping(value="/")
-    public String index()
-    {
-        System.out.println("request received");
-        return "Welcome to First spring boot application java project";
-    }
+
+    // @RequestMapping(value="/")
+    // public String index()
+    // {
+    //     System.out.println("request received");
+    //     return "Welcome to First spring boot application java project";
+    // }   
 }
