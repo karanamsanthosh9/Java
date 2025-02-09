cc19a1ec17500a45eb11a8464ff7c6abcfc8032b
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-02-09 05:37:55 +0000

    commit

---
+++ Projects/first_spring_basic/src/main/java/com/example/first_spring_basic/FirstSpringBasicApplication.java
@@ -0,0 +1,14 @@
+package com.example.first_spring_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class FirstSpringBasicApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(FirstSpringBasicApplication.class, args);
+
+	}
+
+}
