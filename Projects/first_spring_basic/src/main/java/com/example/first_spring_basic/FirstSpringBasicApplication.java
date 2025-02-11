a5bf4760eb027a03f585edd9ac6d2d267aa95519
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-02-11 14:45:23 +0000

    commit

--- Projects/first_spring_basic/src/main/java/com/example/first_spring_basic/FirstSpringBasicApplication.java
+++ Projects/first_spring_basic/src/main/java/com/example/first_spring_basic/FirstSpringBasicApplication.java
@@ -1,13 +1,13 @@
 package com.example.first_spring_basic;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
+@SpringBootApplication
 public class FirstSpringBasicApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(FirstSpringBasicApplication.class, args);
+		SpringApplication.run(FirstSpringBasicApplication.class, args);
 
 	}
 
